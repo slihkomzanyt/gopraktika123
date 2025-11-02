c693c1dd8485ac793e5f46e0f20e4f492001ca8e
slihkomzanyt <rraabboottaa77@ya.ru>
2025-11-02 16:03:01 +0300

    решил вторую задачу

--- main.go
+++ main.go
@@ -1,45 +1,20 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"sort"
 )
 
-// создаем слайз в функции
-func findSecondMax(slice []int) (int, error) {
-	if len(slice) < 2 {
-		return 0, errors.New("что то не так")
-	}
-	//задаем две переменные макс1 и макс2 принимающие значения слайса
-	// если макс1 больше макс2, то сравниваем слайс2 со слайсом 3
-	max1, max2 := slice[0], slice[1]
-	if max2 > max1 {
-		max1, max2 = max2, max1
-	}
+func mergeAndSort(arr1, arr2 []int) []int {
+	rezultat := append(arr1, arr2...)
+	sort.Ints(rezultat)
+	return rezultat
 
-	// Проходим по остальным элементам
-	//для этого задаем переменную i которая каждый раз увеличивается на 1
-	//создаем переменную курент для простоты сравнения и передаем в нее переменную i
-	for i := 2; i < len(slice); i++ {
-		current := slice[i]
-
-		if current > max1 {
-			max2 = max1
-			max1 = current
-		} else if current > max2 && current != max1 {
-			max2 = current
-		}
-	}
-
-	return max2, nil
 }
-
 func main() {
-	slice := []int{3, 5, 7, 1, 9}
-	result, err := findSecondMax(slice)
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Second max:", result)
-	}
+	arr1 := []int{1, 2, 3, 4, 5}
+	arr2 := []int{4, 2, 6, 6, 1}
+	rezultat := mergeAndSort(arr1, arr2)
+	fmt.Println(rezultat)
+
 }
