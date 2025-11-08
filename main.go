5dbbdadce00057a23a1d96f935665b3a880cc311
slihkomzanyt <rraabboottaa77@ya.ru>
2025-11-09 01:45:52 +0300

    revers

--- main.go
+++ main.go
@@ -2,25 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-func reverseNumber(n int) int {
-	if n < 0 {
-		n = -n
+func sequenceGenerator() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
 	}
-	str := strconv.Itoa(n)
-	reversed := ""
 
-	for i := len(str) - 1; i >= 0; i-- {
-		reversed += string(str[i])
-
-	}
-	result, _ := strconv.Atoi(reversed)
-	return result
 }
 
 func main() {
-
-	fmt.Println(reverseNumber(123))
+	next := sequenceGenerator()
+	fmt.Println(next())
 }
