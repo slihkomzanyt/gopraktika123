589e67267547818c9874d409941187b9967caf97
slihkomzanyt <rraabboottaa77@ya.ru>
2025-11-12 14:34:41 +0300

    git commit -m mergetMap

--- main.go
+++ main.go
@@ -4,21 +4,19 @@ import (
 	"fmt"
 )
 
-func mergetMap(m1, m2 map[string]int) map[string]int {
-	mergetMap := make(map[string]int)
-	for key, value := range m1 {
-		mergetMap[key] = value
-	}
-	for key, value := range m2 {
-		mergetMap[key] += value
-	}
-	return mergetMap
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+
+	return r.Width * r.Height
 }
 
 func main() {
 
-	m1 := map[string]int{"apple": 5, "banana": 3}
-	m2 := map[string]int{"banana": 5, "orenge": 3}
-	result := mergetMap(m1, m2)
-	fmt.Println(result)
+	r := Rectangle{Width: 5, Height: 10}
+	area := r.Area()
+	fmt.Println(area)
 }
