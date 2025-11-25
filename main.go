84bf7ea194e4b15e4f9d706b935772ae31788b40
slihkomzanyt <rraabboottaa77@ya.ru>
2025-11-25 19:36:57 +0300

    2й блок задач #10

--- main.go
+++ main.go
@@ -3,23 +3,32 @@ package main
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
 )
 
-func main() {
-	var counter int64
-	var wg sync.WaitGroup
-	wg.Add(5)
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	for i := 1; i <= 5; i++ {
-
-		go func() {
-			defer wg.Done()
-			for j := 1; j <= 1000; j++ {
-				atomic.AddInt64(&counter, 1)
-			}
-		}()
+	for n := range jobs {
+		fmt.Printf("Worker %d is processing number %d\n", id, n)
 	}
+}
+
+func main() {
+
+	numJobs := 20
+	numWorkers := 5
+
+	jobs := make(chan int)
+	var wg sync.WaitGroup
+
+	for i := 1; i <= numWorkers; i++ {
+		wg.Add(1)
+		go worker(i, jobs, &wg)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
 	wg.Wait()
-	fmt.Println("counter =", counter)
 }
