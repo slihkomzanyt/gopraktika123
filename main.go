fd7dba6334437fe8fc51aadc18300d2827e275a9
slihkomzanyt <rraabboottaa77@ya.ru>
2025-11-25 18:35:25 +0300

    2й блок задач #9

--- main.go
+++ main.go
@@ -1,65 +1,25 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"sync"
-	"time"
+	"sync/atomic"
 )
 
-type SafeNumber struct {
-	mu    sync.RWMutex
-	value int
-}
-
-func (s *SafeNumber) Get() int {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	return s.value
-}
-
-func (s *SafeNumber) Set(v int) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.value = v
-}
-
 func main() {
-	num := &SafeNumber{value: 0}
-
+	var counter int64
 	var wg sync.WaitGroup
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	wg.Add(5)
 
-	readers := 5
-	for i := 1; i <= readers; i++ {
-		wg.Add(1)
-		go func(id int) {
+	for i := 1; i <= 5; i++ {
+
+		go func() {
 			defer wg.Done()
-			for {
-				select {
-				case <-ctx.Done():
-					fmt.Printf("reader %d: stop\n", id)
-					return
-				default:
-					v := num.Get()
-					fmt.Printf("reader %d: value = %d\n", id, v)
-					time.Sleep(200 * time.Millisecond)
-				}
+			for j := 1; j <= 1000; j++ {
+				atomic.AddInt64(&counter, 1)
 			}
-		}(i)
+		}()
 	}
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 1; i <= 10; i++ {
-			time.Sleep(500 * time.Millisecond)
-			num.Set(i)
-			fmt.Printf("writer: set value = %d\n", i)
-		}
-		cancel()
-	}()
-
 	wg.Wait()
+	fmt.Println("counter =", counter)
 }
